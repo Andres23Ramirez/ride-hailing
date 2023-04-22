49f7831a464a6439c42890e71ea351cee3fbff76
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-21 22:50:43 -0500

    Feat: add methos to riders controller

--- src/controllers/riders/riders.controller.ts
+++ src/controllers/riders/riders.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put } from '@nestjs/common';
 
 @Controller('riders')
-export class RidersController {}
+export class RidersController {
+  @Post('payment-source')
+  postPaymentSource() {
+    return 'Create a Payment Source';
+  }
+
+  @Post('ride')
+  postRide() {
+    return 'Create a Ride';
+  }
+
+  @Put('start-ride')
+  putStarRide() {
+    return 'Start Ride';
+  }
+}
