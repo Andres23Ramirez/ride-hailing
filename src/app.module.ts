7da986002f7b83c1b1b29dc63cb3fd5306e72119
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-24 11:34:23 -0500

    Feat: create driver and rider services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { RidersController } from './controllers/riders/riders.controller';
 import { DriversController } from './controllers/drivers/drivers.controller';
+import { DriversService } from './services/drivers/drivers.service';
+import { RidersService } from './services/riders/riders.service';
 
 @Module({
   imports: [],
   controllers: [RidersController, DriversController],
-  providers: [],
+  providers: [DriversService, RidersService],
 })
 export class AppModule {}
