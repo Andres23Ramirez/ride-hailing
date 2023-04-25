9b6e1333df0cd267233e41ecbb5dfe455ad26628
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-24 21:56:25 -0500

    Feat: modify requestRide method to use riders service

--- src/controllers/riders/riders.controller.ts
+++ src/controllers/riders/riders.controller.ts
@@ -1,7 +1,11 @@
 import { Controller, Post, Put, Body, Param } from '@nestjs/common';
+import { Ride } from '../../entities/ride.entity';
+import { RidersService } from '../../services/riders/riders.service';
 
 @Controller('riders')
 export class RidersController {
+  constructor(private readonly ridersService: RidersService) {}
+
   @Post('payment-source')
   createPaymentSource(@Body() payload: any) {
     return {
@@ -10,12 +14,21 @@ export class RidersController {
     };
   }
 
-  @Post('ride')
-  postRide(@Body() payload: any) {
-    return {
-      message: 'Create a Ride',
-      payload,
-    };
+  @Post('/rides')
+  requestRide(
+    @Body('startLocationLat') startLocationLat: number,
+    @Body('startLocationLng') startLocationLng: number,
+    @Body('endLocationLat') endLocationLat: number,
+    @Body('endLocationLng') endLocationLng: number,
+    @Body('riderId') riderId: number,
+  ): Ride {
+    return this.ridersService.requestRide(
+      startLocationLat,
+      startLocationLng,
+      endLocationLat,
+      endLocationLng,
+      riderId,
+    );
   }
 
   @Put('start-ride/:id')
