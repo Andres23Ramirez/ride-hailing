6847de1d3de94a835af272b0889d0024afbf44ef
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-25 19:11:32 -0500

    Feat: add types validatios to dtos

--- src/dtos/drivers.dto.ts
+++ src/dtos/drivers.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsLatitude, IsLongitude } from 'class-validator';
+
 export class FinishRideDto {
+  @IsNotEmpty()
+  @IsLatitude()
   readonly endLocationLat: number;
+
+  @IsNotEmpty()
+  @IsLongitude()
   readonly endLocationLng: number;
 }
--- src/dtos/riders.dto.ts
+++ src/dtos/riders.dto.ts
@@ -1,7 +1,23 @@
+import { IsNumber, IsNotEmpty, IsLatitude, IsLongitude } from 'class-validator';
+
 export class RequestRideDto {
+  @IsNotEmpty()
+  @IsLatitude()
   readonly startLocationLat: number;
+
+  @IsNotEmpty()
+  @IsLongitude()
   readonly startLocationLng: number;
+
+  @IsNotEmpty()
+  @IsLatitude()
   readonly endLocationLat: number;
+
+  @IsNotEmpty()
+  @IsLongitude()
   readonly endLocationLng: number;
+
+  @IsNotEmpty()
+  @IsNumber()
   readonly riderId: number;
 }
