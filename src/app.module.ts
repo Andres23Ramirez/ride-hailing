63ab826270efeb720cc919622fd7840a39ef3838
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-26 14:47:30 -0500

    Refactor: move module to rigth folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigAppService } from './config/config.service';
-import { AppConfigModule } from './config.module';
+import { AppConfigModule } from './config/config.module';
 import { ConfigService } from '@nestjs/config';
 import { RidersModule } from './riders/riders.module';
 import { DriversModule } from './drivers/drivers.module';
