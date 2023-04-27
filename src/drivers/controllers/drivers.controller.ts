854f7defc87378b96b71dfa8d2b4c185d61564dc
Rodrigo Andrés Ramírez Aguirre <rodrigo23ramirez@gmail.com>
2023-04-27 01:07:52 -0500

    Feat: add documentation to controllers

--- src/drivers/controllers/drivers.controller.ts
+++ src/drivers/controllers/drivers.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Put, Body, Param, ParseIntPipe } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
 import { FinishRideDto } from 'src/drivers/dtos/drivers.dto';
 import { DriversService } from 'src/drivers/services/drivers.service';
 
+@ApiTags('drivers')
 @Controller('drivers')
 export class DriversController {
   constructor(private readonly driversService: DriversService) {}
 
   @Put('finish-ride/:id')
+  @ApiOperation({
+    summary:
+      'Finish a ride and create a transactions charging the total amount',
+  })
   putFinishRide(
     @Param('id', ParseIntPipe) id: number,
     @Body() payload: FinishRideDto,
--- src/riders/controllers/riders.controller.ts
+++ src/riders/controllers/riders.controller.ts
@@ -1,19 +1,24 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+
 import { RequestRideDto } from 'src/riders/dtos/riders.dto';
 import { PaymentSourceDto } from 'src/riders/dtos/paymentSourceResponse.dto';
 import { Ride } from 'src/riders/entities/ride.entity';
 import { RidersService } from 'src/riders/services/riders.service';
 
+@ApiTags('riders')
 @Controller('riders')
 export class RidersController {
   constructor(private readonly ridersService: RidersService) {}
 
   @Post('payment-source')
+  @ApiOperation({ summary: 'Create a payment source for a rider' })
   createPaymentSource(@Body() payload: PaymentSourceDto) {
     return this.ridersService.createPaymentSource(payload);
   }
 
   @Post('/rides')
+  @ApiOperation({ summary: 'Request a ride' })
   requestRide(@Body() payload: RequestRideDto): Ride {
     return this.ridersService.requestRide(payload);
   }
